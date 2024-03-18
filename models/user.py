58a314ec427dc7581dc7f558a6324087cc50457e
Eltrone <amineelorche@gmail.com>
2024-03-18 16:51:57 -0700

    file added

--- models/user.py
+++ models/user.py
@@ -5,6 +5,7 @@ from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
