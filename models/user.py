85396ad4073e574ee16ff0227349a7733694a3c6
Eltrone <amineelorche@gmail.com>
2024-03-18 16:38:35 -0700

    file added

--- models/user.py
+++ models/user.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """user class"""
-from models.base_model import BaseModel
 
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-class User(BaseModel):
-    """create the class user
-    """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class User(BaseModel, Base):
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
