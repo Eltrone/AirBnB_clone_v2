3872749ecffb01d2152cb634cf3036d819b2e5c3
Eltrone <amineelorche@gmail.com>
2024-03-18 17:59:30 -0700

    file added

--- models/place.py
+++ models/place.py
@@ -2,9 +2,9 @@
 """ Module for Place class """
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
-
+from models.amenity import Amenity
 
 class Place(BaseModel, Base):
     """ Place class that inherits from BaseModel """
@@ -23,3 +23,9 @@ class Place(BaseModel, Base):
     cities = relationship("City", back_populates="places")
     reviews = relationship("Review", back_populates="place", cascade="all, delete")
     amenity_ids = []
+    place_amenity = Table('place_amenity', Base.metadata,
+        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+        Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+    amenities = relationship(
+        "Amenity", secondary="place_amenity", 
+        back_populates="place_amenities", viewonly=False)
