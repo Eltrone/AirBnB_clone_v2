524232a4fcf9d932c6da90b468d7d98c152fe6a2
Eltrone <amineelorche@gmail.com>
2024-03-18 18:18:23 -0700

    file added

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -8,6 +8,8 @@ from models.city import City
 from models.user import User
 from models.place import Place
 from models.review import Review
+from models.amenity import Amenity
+
 
 class DBStorage:
     __engine = None
