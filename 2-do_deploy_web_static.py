b20be7a451435c79865f73428b46b41e0eff7c54
Eltrone <amineelorche@gmail.com>
2024-04-04 17:14:47 -0700

    file added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,38 +1,32 @@
 #!/usr/bin/python3
-"""
-Fabric script to distribute an archive to web servers, using do_deploy.
-"""
-from fabric.api import put, run, env
-from os.path import exists
+""" distributes an archive to your web servers """
 
-# Utiliser les adresses IP fournies pour les serveurs
-env.hosts = ['34.224.62.139', '100.25.119.231']  
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
+import os.path
+
+env.user = 'ubuntu'
+env.hosts = ["34.224.62.139", "100.25.119.231"]
+env.key_filename = "~/.ssh/id_rsa"
+
 
 def do_deploy(archive_path):
-    """Distributes an archive to web servers."""
-    if not exists(archive_path):
+    """ do_deploy fonction """
+    if os.path.exists(archive_path) is False:
         return False
-
     try:
-        # Upload the archive
+        arc = archive_path.split("/")
+        base = arc[1].strip('.tgz')
         put(archive_path, '/tmp/')
-
-        # Extract file name and prepare directory paths
-        file_name = archive_path.split("/")[-1]
-        file_wo_ext = file_name.split(".")[0]
-        full_dir_path = "/data/web_static/releases/" + file_wo_ext + "/"
-
-        # Commands to deploy the archive
-        run("mkdir -p " + full_dir_path)
-        run("tar -xzf /tmp/" + file_name + " -C " + full_dir_path)
-        run("rm /tmp/" + file_name)
-        mv_cmd = "mv " + full_dir_path + "web_static/* " + full_dir_path
-        run(mv_cmd)
-        run("rm -rf /data/web_static/current")
-        run("ln -s " + full_dir_path + " /data/web_static/current")
-
-        print("New version deployed!")
+        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        main = "/data/web_static/releases/{}".format(base)
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
-    except Exception:
-        return False
 
+    except:
+        return False
