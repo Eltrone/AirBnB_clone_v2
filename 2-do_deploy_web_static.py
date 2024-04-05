e369bc764b2bb60a198d93ed212d165e51bd67a6
Eltrone <amineelorche@gmail.com>
2024-04-04 17:28:38 -0700

    file added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
 """ distributes an archive to your web servers """
 
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
-import os.path
+from fabric.api import *
+from os import path
 
 env.user = 'ubuntu'
 env.hosts = ["34.224.62.139", "100.25.119.231"]
 env.key_filename = "~/.ssh/id_rsa"
 
-
 def do_deploy(archive_path):
-    """ do_deploy fonction """
-    if os.path.exists(archive_path) is False:
+    """
+    Distributes an archive to your web servers
+    """
+    if not path.exists(archive_path):
         return False
-    try:
-        arc = archive_path.split("/")
-        base = arc[1].strip('.tgz')
-        put(archive_path, '/tmp/')
-        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
-        main = "/data/web_static/releases/{}".format(base)
-        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
-        sudo('rm /tmp/{}'.format(arc[1]))
-        sudo('mv {}/web_static/* {}/'.format(main, main))
-        sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
-        return True
 
+    filename = archive_path.split('/')[-1]
+    foldername = '/data/web_static/releases/' + filename.split('.')[0]
+
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(foldername))
+        run("tar -xzf /tmp/{} -C {}".format(filename, foldername))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}/web_static/* {}".format(foldername, foldername))
+        run("rm -rf {}/web_static".format(foldername))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(foldername))
+        print("New version deployed!")
+        return True
     except:
         return False
