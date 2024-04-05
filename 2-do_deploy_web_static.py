c703390a54c14d6c881c52bf65a9587b8c8006ab
Eltrone <amineelorche@gmail.com>
2024-04-04 18:38:53 -0700

    file added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-""" distributes an archive to your web servers """
+"""
+Fabric script that distributes an archive to web servers
+"""
 
-from fabric.api import *
-from os import path
+from fabric.api import put, run, env
+from os.path import exists
 
-env.user = 'ubuntu'
 env.hosts = ["34.224.62.139", "100.25.119.231"]
-env.key_filename = "~/.ssh/id_rsa"
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to your web servers
-    """
-    if not path.exists(archive_path):
+    """Distributes an archive to web servers"""
+    if not exists(archive_path):
         return False
-
-    filename = archive_path.split('/')[-1]
-    foldername = '/data/web_static/releases/' + filename.split('.')[0]
-
     try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(foldername))
-        run("tar -xzf /tmp/{} -C {}".format(filename, foldername))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}/web_static/* {}".format(foldername, foldername))
-        run("rm -rf {}/web_static".format(foldername))
+        # Upload the archive
+        put(archive_path, '/tmp/')
+        archive_filename = archive_path.split("/")[-1]
+        without_ext = archive_filename.split(".")[0]
+        release_path = f"/data/web_static/releases/{without_ext}/"
+        # Uncompress the archive
+        run(f"mkdir -p {release_path}")
+        run(f"tar -xzf /tmp/{archive_filename} -C {release_path}")
+        run(f"rm /tmp/{archive_filename}")
+        # Move the content to the correct location
+        run(f"mv {release_path}web_static/* {release_path}")
+        run(f"rm -rf {release_path}web_static")
+        # Delete the current symbolic link and create a new one
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(foldername))
+        run(f"ln -s {release_path} /data/web_static/current")
         print("New version deployed!")
         return True
     except:
---
+++ 3-deploy_web_static.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Fabric script that creates and distributes an archive to web servers
+"""
+
+from fabric.api import local, put, run, env
+from datetime import datetime
+import os
+
+env.hosts = ["34.224.62.139", "100.25.119.231"]
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder."""
+    try:
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        archive_path = "versions/web_static_{}.tgz".format(now)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except Exception as e:
+        return None
+
+def do_deploy(archive_path):
+    """Distributes an archive to web servers"""
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        archive_filename = archive_path.split("/")[-1]
+        without_ext = archive_filename.split(".")[0]
+        release_path = "/data/web_static/releases/{}/".format(without_ext)
+        put(archive_path, '/tmp/')
+        run("mkdir -p {}".format(release_path))
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_path))
+        run("rm /tmp/{}".format(archive_filename))
+        run("mv {}web_static/* {}".format(release_path, release_path))
+        run("rm -rf {}web_static".format(release_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(release_path))
+        print("New version deployed!")
+        return True
+    except Exception as e:
+        return False
+
+def deploy():
+    """Creates and distributes an archive to web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
