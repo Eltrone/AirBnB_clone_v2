9ebf2f6eb04e757c77a8922daaaacad10baa41ae
Eltrone <amineelorche@gmail.com>
2024-04-06 08:42:18 -0700

    file added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to web servers
-"""
-
+"""Un module pour distribuer une archive aux serveurs et la déployer"""
 from fabric.api import put, run, env
-from os.path import exists
+import os
 
-env.hosts = ["34.224.62.139", "100.25.119.231"]
+env.hosts = ['52.23.177.252', '18.204.7.7']
+env.user = "ubuntu"
 
 def do_deploy(archive_path):
-    """Distributes an archive to web servers"""
-    if not exists(archive_path):
+    """Fonction pour déployer l'archive"""
+    if not os.path.exists(archive_path):
         return False
+    
     try:
-        # Upload the archive
-        put(archive_path, '/tmp/')
-        archive_filename = archive_path.split("/")[-1]
-        without_ext = archive_filename.split(".")[0]
-        release_path = f"/data/web_static/releases/{without_ext}/"
-        # Uncompress the archive
-        run(f"mkdir -p {release_path}")
-        run(f"tar -xzf /tmp/{archive_filename} -C {release_path}")
-        run(f"rm /tmp/{archive_filename}")
-        # Move the content to the correct location
-        run(f"mv {release_path}web_static/* {release_path}")
-        run(f"rm -rf {release_path}web_static")
-        # Delete the current symbolic link and create a new one
+        aname = os.path.basename(archive_path)
+        rname = aname.split(".")[0]
+        
+        # Transférer l'archive vers le dossier /tmp/ du serveur
+        put(archive_path, "/tmp/")
+        
+        # Décompresser l'archive dans /data/web_static/releases/
+        run(f"mkdir -p /data/web_static/releases/{rname}")
+        run(f"tar -xzf /tmp/{aname} -C /data/web_static/releases/{rname}/")
+        
+        # Supprimer l'archive du serveur
+        run(f"rm /tmp/{aname}")
+        
+        # Supprimer le lien symbolique actuel et en créer un nouveau
         run("rm -rf /data/web_static/current")
-        run(f"ln -s {release_path} /data/web_static/current")
+        run(f"ln -s /data/web_static/releases/{rname}/ /data/web_static/current")
+        
+        # Nettoyage: déplacer les fichiers de web_static vers le dossier parent et supprimer web_static
+        run(f"mv /data/web_static/current/web_static/* /data/web_static/current/")
+        run("rm -rf /data/web_static/current/web_static")
+        
         print("New version deployed!")
         return True
     except:
