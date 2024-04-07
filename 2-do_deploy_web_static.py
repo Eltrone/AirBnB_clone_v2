a0ef1ac0c6ad8847bd13b40d6a11400f68f39979
Eltrone <amineelorche@gmail.com>
2024-04-07 11:26:16 -0700

    type: localforage pages recherche & admin modif data firestore

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""
-Distribue une archive à vos serveurs web
-"""
+""" a module to push a package to servers and deploy """
+import os
+from fabric.api import put, env, run
 
-from fabric.api import env, put, run
-import os.path
-
-env.user = 'ubuntu'
 env.hosts = ["34.224.62.139", "100.25.119.231"]
-env.key_filename = "~/.ssh/id_rsa"
+
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """
-    Déploie l'archive sur les serveurs
-    """
-    if not os.path.exists(archive_path):
+    """ deploy package """
+    if archive_path is None or not os.path.isfile(archive_path):
         return False
-    try:
-        arc = archive_path.split("/")
-        base = arc[1].strip('.tgz')
-        put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}'.format(base))
-        main = "/data/web_static/releases/{}".format(base)
-        run('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
-        run('rm /tmp/{}'.format(arc[1]))
-        run('mv {}/web_static/* {}/'.format(main, main))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}/ "/data/web_static/current"'.format(main))
-        return True
 
-    except:
-        return False
+    aname = os.path.basename(archive_path)
+    rname = aname.split(".")[0]
+
+    put(local_path=archive_path, remote_path="/tmp/")
+    run("mkdir -p /data/web_static/releases/{}".format(rname))
+    run("tar -xzf /tmp/{} \
+        -C /data/web_static/releases/{}".format(aname, rname))
+    run("rm /tmp/{}".format(aname))
+    run("rm -rf /data/web_static/current")
+    run("ln -fs /data/web_static/releases/{}/ \
+        /data/web_static/current".format(rname))
+    run("mv /data/web_static/current/web_static/* /data/web_static/current/")
+    run("rm -rf /data/web_static/current/web_static")
+
+    return True
