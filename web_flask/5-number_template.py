28f431fbf662b704ab25f4ef79c28cb4d99780d6
Eltrone <amineelorche@gmail.com>
2024-04-20 22:56:29 +0000

    file added

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Starts a Flask web app with several routes. """
-from flask import Flask
+""" Starts a Flask web app with routes including an HTML template response. """
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -34,6 +34,12 @@ def python_text(text):
 def number(n):
     """ Returns 'n is a number' only if n is an integer. """
     return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ Renders an HTML page only if n is an integer. """
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
