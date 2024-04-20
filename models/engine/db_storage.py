40b160d0a771057e77dd5ab1c96bb75c7848eaef
Eltrone <amineelorche@gmail.com>
2024-04-20 23:23:10 +0000

    file added

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -60,3 +60,7 @@ class DBStorage:
             bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """Closes the current SQLAlchemy session."""
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 """ Module for State class """
 
+from os import getenv
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+from models import storage_type  # Supposons que vous avez défini cela quelque part dans le module de configuration
 
 class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+
+    if storage_type == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """Return the list of City objects from storage linked to the current State."""
+            from models import storage
+            from models.city import City
+            all_cities = storage.all(City)
+            return [city for city in all_cities.values() if city.state_id == self.id]
