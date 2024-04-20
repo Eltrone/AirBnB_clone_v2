92398e1a669686a2e39b5365f85d4e97a07a0d35
Eltrone <amineelorche@gmail.com>
2024-04-20 23:01:01 +0000

    file added

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,7 +33,7 @@ def python_text(text):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """ Returns 'n is a number' only if n is an integer. """
-    return f"{n} is a number"
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
