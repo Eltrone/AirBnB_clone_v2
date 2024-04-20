7532acfcd2e2eb73b357e992eddba7e5139ab871
Eltrone <amineelorche@gmail.com>
2024-04-20 23:06:30 +0000

    file added

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,46 +1,43 @@
 #!/usr/bin/python3
 """ Starts a Flask web app with routes including an HTML template response. """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Returns 'Hello HBNB!' at the root URL. """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Returns 'HBNB' at the URL /hbnb. """
-    return "HBNB"
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """ Returns 'C ' followed by the text, underscores replaced by spaces. """
-    return 'C ' + text.replace('_', ' ')
+    ftesxt = text.replace('_', ' ')
+    return 'C {}'.format(ftesxt)
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    """ Returns 'Python ' followed by text, underscores replaced by spaces. """
-    return 'Python ' + text.replace('_', ' ')
+    ftesxt = text.replace('_', ' ')
+    return 'Python {}'.format(ftesxt)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """ Returns 'n is a number' only if n is an integer. """
-    return "{} is a number".format(n)
+    return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """ Renders an HTML page only if n is an integer. """
-    return render_template('5-number.html', n=n)
+    return render_template("5-number.html", n=n)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
