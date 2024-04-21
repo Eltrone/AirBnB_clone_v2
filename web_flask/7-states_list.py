c8eaf0e9be99e4380a31bfd311267c9495d9f7e6
Eltrone <amineelorche@gmail.com>
2024-04-21 00:17:49 +0000

    file added

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 """Starts a Flask web application with listing of states."""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
 
 
+@app.route('/states_list', methods=['GET'], strict_slashes=False)
+def states_list():
+    states = storage.all("State")
+    if states:
+        sorted_states = sorted(states.values(), key=lambda state: state.name)
+    else:
+        sorted_states = []
+    return render_template('7-states_list.html', states=sorted_states)
+
+
 @app.teardown_appcontext
-def teardown(exception):
+def teardown_db(exception):
+    """Ferme la session de base de données."""
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list(sorted_states=None):
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=sorted_states)
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
